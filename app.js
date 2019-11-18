b7628ceec96fea7033f44c58244420c05bdbf0d4
cakoshakib <cakoshakib@gmail.com>
2019-11-17 23:23:32 -0500

    responsive nav, some responsive stuff

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelector(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle('open');
+    
+});
