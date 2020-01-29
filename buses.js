5e582ff2c736522c8089931ef688fffdf4410a28
hybras <24651269+hybras@users.noreply.github.com>
2020-01-29 18:43:05 -0500

    Refactor SignOut func to buses.js

--- buses.js
+++ buses.js
@@ -39,6 +39,13 @@ function onSignIn(googleUser) {
 	document.getElementById('EMail').value = Buses.profile.getEmail();
 }
 
+function signOut() {
+	const auth2 = gapi.auth2.getAuthInstance()
+	auth2.signOut().then(function () {
+		console.log('User signed out.')
+	})
+}
+
 function init() {
 	Buses.init()
 }
